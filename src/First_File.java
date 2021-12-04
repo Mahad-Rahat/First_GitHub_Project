f7a8f2ae3831558284ab4c8de5558b3a8c04ee7c
Mahad-Rahat <95528513+Mahad-Rahat@users.noreply.github.com>
2021-12-04 16:42:21 +0500

    Update First_File.java

--- src/First_File.java
+++ src/First_File.java
@@ -2,7 +2,7 @@
 public class First_File {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String name = "Mahad";
+		String name = "Mahad Rahat";
 		String rollno = "20i-1808";
 		System.out.println("Name: " + name);
 		System.out.println("RollNo: " + rollno);
